5e6bae5c9966da1128bf609526c6a5c16ab6375e
José Calderón <jgcalderon372@gmail.com>
2024-06-21 12:33:50 -0500

    Primer commit con el proyecto base para crear el juego

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+O
+src/main/java/JuegoMemoria.java,3\3\33968adaeda6d27f130133e5b3deadd319ba59a3
---
+++ src/main/java/JuegoMemoria.java
@@ -0,0 +1,15 @@
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class JuegoMemoria extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+        gameSettings.setWidth(800);
+        gameSettings.setHeight(600);
+        gameSettings.setTitle("Juego de Memoria");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
